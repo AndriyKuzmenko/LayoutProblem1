424c37b59dd077f39eb076c06039bc303c20738a
Andriy <ak8022@bs.amalnet.k12.il>
2020-03-03 09:20:46 +0200

    TextView

--- app/src/main/java/com/example/layoutproblem1/MainActivity.java
+++ app/src/main/java/com/example/layoutproblem1/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.layoutproblem1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    TextView myTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myTextView=(TextView)findViewById(R.id.textView);
     }
 }
