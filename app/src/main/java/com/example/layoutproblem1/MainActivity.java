0034441ff6082b3c23f2bb87b6f63a5258b68653
Andriy <ak8022@bs.amalnet.k12.il>
2020-03-03 09:26:44 +0200

    import Random

--- app/src/main/java/com/example/layoutproblem1/MainActivity.java
+++ app/src/main/java/com/example/layoutproblem1/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.layoutproblem1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.Random;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -9,6 +9,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity
 {
     TextView myTextView;
+    Random myRandom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         myTextView=(TextView)findViewById(R.id.textView);
+        myRandom=new Random();
     }
 
     public void Clear(View view)
